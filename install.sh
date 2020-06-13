a94c172c79388590809904c44c16fdd951969a7d
Dave0x21 <galassodavide@protonmail.com>
2020-06-13 23:03:10 +0200

    Add install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo("Installing PiTemp...")
+
+sudo mkdir -pv /etc/pitemp
+sudo mkdir -pv /var/www/pitemp
+
+sudo cp  -r static templates /var/www/pitemp
+sudo cp  -r icons /etc/pitemp
+sudo cp pitemp /usr/local/bin
+sudo cp pitemp_server /var/www/pitemp

c3a27eaba678d02c221c83e0c66a8bc3d6039ac6
Dave0x21 <galassodavide@protonmail.com>
2020-06-14 12:33:56 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,3 +9,7 @@ sudo cp  -r static templates /var/www/pitemp
 sudo cp  -r icons /etc/pitemp
 sudo cp pitemp /usr/local/bin
 sudo cp pitemp_server /var/www/pitemp
+
+sudo cp service/* /etc/systemd/system
+sudo systemctl enable --now pitemp
+sudo systemctl enable --now pitemp_server
